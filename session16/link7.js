2f74930ccf0752c1364405555705f0b7d1a6175c
TamakiAko2006 <TamakiAko2006@gmail.com>
2025-02-26 17:00:33 +0700

    link7.js

--- session16/link7.js
+++ session16/link7.js
@@ -2,4 +2,4 @@ let Math = +prompt("mời bạn nhập điểm Toán");
 let Physics = +prompt("mời bạn nhập điểm Lý");
 let Chemistry = +prompt("mời bạn bạn nhập điểm Hóa");
 let diemTB = (Math + Physics + Chemistry) / 3;
-alert("điểm trung bình là "+diemTB);
+document.write("điểm trung bình là "+diemTB);
